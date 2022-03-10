252073e6e6ae1f46d465589b2bf64b79d9fe477c
Farid Amirul <faridamirul268@gmail.com>
2022-03-10 14:32:56 +0700

    Ini Program WhiteSpaces

---
+++ src/main/kotlin/WhiteSpaces.kt
@@ -0,0 +1,5 @@
+//WhiteSpace pada kotlin tidak signifikan mempengaruhi program dan dengan
+// aman dapat diacuhkan.kita juga bisa menggunakan gaya penulisan sebagai berikut :
+fun main() {
+                                                                                       println("Halllo Ini Code Program Pertama saya!!!")
+}
