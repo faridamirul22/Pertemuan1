14944f06bc7f5702210db35380817ae338668487
Farid Amirul <faridamirul268@gmail.com>
2022-03-10 14:41:18 +0700

    Ini Program Blok dan Komentar

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,21 @@
+/*
+Blok digunakan untuk menulis ikatan sebuah statment yang di kelompokkan menjadi satu bagian. COntoh :
+
+fun main() {
+    println("ini Kode program pertama saya")
+}
+ */
+
+fun main() {
+    println("Ini program Kotlin Pertama saya")
+}
+
+// contoh komentar satu baris
+
+/*
+contoh komentar multi baris
+ */
+
+/**
+ * kotlin Doc --> ini baris untukk mendokumentasikan kode program
+ */
