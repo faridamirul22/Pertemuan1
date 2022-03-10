d09566e9027b2183e34bd322cd52c82f51491c96
Farid Amirul <faridamirul268@gmail.com>
2022-03-10 15:21:21 +0700

    Ini Program ConstantLiteralll

---
+++ src/main/kotlin/ConstantsLiteral.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val floatLiteral : Float = 178.95F // literal Float
+    println("Contoh literal float = $floatLiteral")
+    val  oneBillion: Long = 1000000000L //Literal Long
+    println("contoh Literal Long : $oneBillion")
+    val valHeksa = 0x0F // heksa desimal diawali dengan 0x
+    println("Contoh Literal Hekasdesmial : $valHeksa")
+    val  valBinary = 0b00001011// literal  diawali dengan 0 b
+    println("vontoh literal Binary : $valBinary")
+    val  notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional floating Point Number : $notasiKonvensional")
+}
