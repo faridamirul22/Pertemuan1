c595d1ed9679c65662fc72bbb001b4faf4d644cd
Farid Amirul <faridamirul268@gmail.com>
2022-03-10 14:51:35 +0700

    Ini Program Var Variable

---
+++ src/main/kotlin/var&val.kt
@@ -0,0 +1,15 @@
+/* Var Variable
+Kata kunci var di Kotlin digunakan untuk mendeklarasikan variabel yang nilainya dapat berubah
+sepanjang program dieksekusi.
+ */
+
+fun main() {
+    var varVariable: Int
+    varVariable = 56 // penugasan value pertama ke varVariable
+    println("Pemberian nilai pertama pada varVariable = $varVariable")
+    varVariable = 78 // pemberian nilai kedua ke varVariable
+    println("Pemberian nilai kedua pada varVAriable")
+
+
+}
+
