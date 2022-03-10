3356d44bd1135093ce1682e8680907f351264644
Farid Amirul <faridamirul268@gmail.com>
2022-03-10 14:27:16 +0700

    Ini Program Literal

---
+++ src/main/kotlin/literal.kt
@@ -0,0 +1,13 @@
+//Literal memberikan tipe data secara otomatis setelah program di kompilasi sesuai dengan nili yang cocok untuk di berikan pada varialbel
+fun main() {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hallo"
+    var charLiteral = '1'
+    var boolLiteral = true
+    println("intLeteran = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringliteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+}
